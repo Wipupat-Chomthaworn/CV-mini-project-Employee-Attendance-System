820a2c0bf8c4c607c89963cd74dae6b4613c582a
Owen <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2023-04-23 15:31:29 +0700

    Update REQ for LBPH.py

--- LBPH.py
+++ LBPH.py
@@ -42,8 +42,10 @@ while True:
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the image
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(
+        gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     # Loop through each face found in the frame and check if it matches a known face
     for (x, y, w, h) in faces:
@@ -58,7 +60,8 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
         # Write the name of the person on the rectangle
-        cv2.putText(frame, name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, name, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
